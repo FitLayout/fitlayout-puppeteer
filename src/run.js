d84f1df780ab23c5ab2c5fea0c85a6850ba6db21
Radek Burget <burgetr@fit.vutbr.cz>
2020-11-14 23:01:10 +0100

    Command line interface added

--- src/run.js
+++ src/run.js
@@ -6,20 +6,32 @@
  * parsed by fitlayout-render-puppeteer.
  */
 
+const err = process.stderr;
+if (process.argv.length !== 5) {
+	err.write('Usage: node run.js <url> <width> <height>\n');
+	err.write('Where:\n')
+	err.write('    <url> is the URL of the page to render\n');
+	err.write('    <width> is the browser window width\n')
+	err.write('    <height> is the browser window width\n')
+	process.exit(1);
+}
+
+const targetUrl = process.argv[2];
+const wwidth = process.argv[3];
+const wheight = process.argv[4];
+
 const puppeteer = require('puppeteer');
 
 (async () => {
 	const browser = await puppeteer.launch({
 		headless: false,
 		//slowMo: 250,
-		args: [`--window-size=1600,1200`],
+		args: [`--window-size=${wwidth},${wheight}`],
 		defaultViewport: null
 	});
 	const page = await browser.newPage();
-	//await page.goto('https://www.fit.vut.cz/study/courses/');
-	await page.goto('http://cssbox.sf.net');
-	//await page.goto('https://www.idnes.cz/technet/software/bezpecnostni-chyba-prohlizec-google-chrome-instalujte-aktualizaci.A201104_174236_software_nyv');
-	//page.on('console', msg => console.log('PAGE LOG:', msg.text()));
+	await page.goto(targetUrl);
+	//page.on('console', msg => err.write('PAGE LOG:', msg.text() + '\n'));
 
 	let pg = await page.evaluate(() => {
 
