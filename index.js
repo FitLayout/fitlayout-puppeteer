d7053d1af9cff4f1870595051c8b066022aee41b
Radek Burget <burgetr@fit.vutbr.cz>
2020-11-18 09:13:35 +0100

    Improved CLI

--- index.js
+++ index.js
@@ -6,19 +6,33 @@
  * parsed by fitlayout-render-puppeteer.
  */
 
-const err = process.stderr;
-if (process.argv.length !== 5) {
-	err.write('Usage: node run.js <url> <width> <height>\n');
-	err.write('Where:\n')
-	err.write('    <url> is the URL of the page to render\n');
-	err.write('    <width> is the browser window width\n')
-	err.write('    <height> is the browser window width\n')
+const argv = require('yargs/yargs')(process.argv.slice(2))
+    .usage('Usage: $0 [options] <url>')
+	//.example('$0 -w 1200 -h 800 http://cssbox.sf.net', '')
+	.strictOptions(true)
+    .alias('W', 'width')
+    .nargs('W', 1)
+	.default('W', 1200)
+	.describe('W', 'Target page width')
+    .alias('H', 'height')
+    .nargs('H', 1)
+	.default('H', 800)
+	.describe('H', 'Target page height')
+	.alias('s', 'screenshot')
+	.boolean('s')
+	.describe('s', 'Include a screenshot in the result')
+    .help('h')
+    .alias('h', 'help')
+    .argv;
+
+if (argv._.length !== 1) {
+	process.stderr.write('<url> is required. Use -h for help.\n');
 	process.exit(1);
 }
 
-const targetUrl = process.argv[2];
-const wwidth = process.argv[3];
-const wheight = process.argv[4];
+const targetUrl = argv._[0];
+const wwidth = argv.width;
+const wheight = argv.height;
 
 const puppeteer = require('puppeteer');
 
